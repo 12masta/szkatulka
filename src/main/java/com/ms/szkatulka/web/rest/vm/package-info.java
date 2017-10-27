dae52d9977e34a85b675c53528ff13e52524445f
12masta <stanek.marcinp@gmail.com>
2017-10-27 23:43:30 +0200

    Generated with JHipster 4.10.0

---
+++ src/main/java/com/ms/szkatulka/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.ms.szkatulka.web.rest.vm;
---
+++ src/main/webapp/app/shared/shared-common.module.ts
@@ -0,0 +1,36 @@
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+import {
+    SzkatulkaSharedLibsModule,
+    JhiLanguageHelper,
+    FindLanguageFromKeyPipe,
+    JhiAlertComponent,
+    JhiAlertErrorComponent
+} from './';
+
+@NgModule({
+    imports: [
+        SzkatulkaSharedLibsModule
+    ],
+    declarations: [
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ],
+    providers: [
+        JhiLanguageHelper,
+        Title,
+        {
+            provide: LOCALE_ID,
+            useValue: 'pl'
+        },
+    ],
+    exports: [
+        SzkatulkaSharedLibsModule,
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ]
+})
+export class SzkatulkaSharedCommonModule {}
